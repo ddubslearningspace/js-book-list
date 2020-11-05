fc948b37dd848f3d11c59495fee38511636f43e7
Daniel Warner <52971033+ddubthedev@users.noreply.github.com>
2020-11-04 17:37:45 -0700

    added local storage

--- appes6.js
+++ appes6.js
@@ -59,6 +59,53 @@ class UI {
 	}
 }
 
+// Local storage class
+class Store {
+	static getBooks() {
+		let books;
+		if (localStorage.getItem("books") === null) {
+			books = [];
+		} else {
+			books = JSON.parse(localStorage.getItem("books"));
+		}
+
+		return books;
+	}
+
+	static displayBooks() {
+		const books = Store.getBooks();
+
+		books.forEach(function (book) {
+			// instantiate ui
+			const ui = new UI();
+
+			// Add book to UI
+			ui.addBookToList(book);
+		});
+	}
+
+	static addBook(book) {
+		const books = Store.getBooks();
+		books.push(book);
+		localStorage.setItem("books", JSON.stringify(books));
+	}
+
+	static removeBook(isbn) {
+		const books = Store.getBooks();
+
+		books.forEach(function (book, index) {
+			if (book.isbn === isbn) {
+				books.splice(index, 1);
+			}
+		});
+
+		localStorage.setItem("books", JSON.stringify(books));
+	}
+}
+
+// Load DOM event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
 // Event Listeners
 document.getElementById("book-form").addEventListener("submit", function (e) {
 	// Get form values
@@ -80,6 +127,9 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
 		// Add book to list
 		ui.addBookToList(book);
 
+		// Add to local storage
+		Store.addBook(book);
+
 		// Show success
 		ui.showAlert("Book added!", "success");
 
@@ -95,8 +145,11 @@ document.getElementById("book-list").addEventListener("click", function (e) {
 	// Instantiate UI
 	const ui = new UI();
 
-	// call delete meathod
+	// Delete Book
 	ui.deleteBook(e.target);
+
+	// Remove from local storage
+	Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
 	// Show message
 	ui.showAlert("Book removed!", "success");
